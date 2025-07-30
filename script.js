8a347b20f039aac17a4a75e884b0612bb6731967
Alexandre <coralexandre36@gmail.com>
2025-07-30 12:42:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,46 @@
+let currentTheme = null;
+let keepVisible = false;
+
 function showInfo(theme) {
+  currentTheme = theme;
+  keepVisible = false;
+
   const infoBox = document.getElementById("infoBox");
   const infoImg = document.getElementById("infoImg");
   const infoText = document.getElementById("infoText");
+  const infoLink = document.getElementById("infoLink");
 
   const data = {
     avion: {
       img: "img/avion.jpg",
       text: "Design et aérodynamique des maquettes volantes.",
+      link: "projets/avion.html"
     },
     sub: {
       img: "img/submarine.jpg",
       text: "Exploration des systèmes immergés et propulsion sous-marine.",
+      link: "projets/sous-marin.html"
     },
     bateau: {
       img: "img/boat.jpg",
       text: "Modélisation navale et stabilité en milieu aquatique.",
-    },
+      link: "projets/bateau.html"
+    }
   };
 
   infoImg.src = data[theme].img;
   infoText.textContent = data[theme].text;
+  infoLink.href = data[theme].link;
+
   infoBox.style.display = "block";
 }
 
+function keepInfoVisible() {
+  keepVisible = true;
+}
+
 function hideInfo() {
-  document.getElementById("infoBox").style.display = "none";
+  if (!keepVisible) {
+    document.getElementById("infoBox").style.display = "none";
+  }
 }
